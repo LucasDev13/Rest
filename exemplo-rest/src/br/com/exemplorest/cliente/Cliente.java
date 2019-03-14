52f00a827b2ddcdb19450b55cce7c636e24ed408
Harmfull <Harmfull@DESKTOP-FE5PL37>
2019-03-13 23:21:19 -0300

    commit- criado construtor e métodos getter e setter

--- exemplo-rest/src/br/com/exemplorest/cliente/Cliente.java
+++ exemplo-rest/src/br/com/exemplorest/cliente/Cliente.java
@@ -1,9 +1,45 @@
 package br.com.exemplorest.cliente;
 
 public class Cliente {
-  private Long id;
-  private String nome;
-  private String sobrenome;
-  private int idade;
+	private Long id;
+	private String nome;
+	private String sobrenome;
+	private int idade;
+
+	public Cliente() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
 
 }
